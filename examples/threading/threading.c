19713209bc43c3c8dbbf5c69f4568f026983060d
vkonovets <slava.konovets@yahoo.com>
2023-01-21 12:27:53 +0100

    test commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,6 +15,14 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data*)thread_param;
+
+    timespec req = {
+        .tv_sec = thread_func_args->wait_to_obtain_ms / 1000,
+        .tv_nsec = (thread_func_args->wait_to_obtain_ms % 1000) * 1000000
+    };
+    
+
     return thread_param;
 }
 
@@ -28,6 +37,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    
+    thread_data* data = (struct thread_data*)malloc(sizeof(struct thread_data));
+    data->wait_to_obtain_ms = wait_to_release_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread = thread;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0)
+    {
+        printf("Attempt to pthread_create failed with %d\n.", rc);
+        return false;
+    }
+    
+    return true;
 }
 
