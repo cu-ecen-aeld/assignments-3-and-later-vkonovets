8c218e768acc321e821b4f9f2b4b18905d8a588d
vkonovets <slava.konovets@yahoo.com>
2023-01-22 21:46:50 +0100

    Do not attempt to remove writedir

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -30,25 +30,25 @@ MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines a
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
-rm -rf "${WRITEDIR}"
-
-# create $WRITEDIR if not assignment1
-assignment=`cat /etc/finder-app/conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
-fi
+# rm -rf "${WRITEDIR}"
+# 
+# # create $WRITEDIR if not assignment1
+# assignment=`cat /etc/finder-app/conf/assignment.txt`
+# 
+# if [ $assignment != 'assignment1' ]
+# then
+# 	mkdir -p "$WRITEDIR"
+# 
+# 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+# 	#The quotes signify that the entire string in WRITEDIR is a single string.
+# 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+# 	if [ -d "$WRITEDIR" ]
+# 	then
+# 		echo "$WRITEDIR created"
+# 	else
+# 		exit 1
+# 	fi
+# fi
 
 #echo "Removing the old writer utility and compiling as a native application"
 #make clean
