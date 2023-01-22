6c30be99dd9f1ac56269062948fc09a574a10408
vkonovets <slava.konovets@yahoo.com>
2023-01-22 21:59:32 +0100

    Correct finder output dir and direct stdout to tmp for result msgs

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,7 +7,7 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/
+WRITEDIR=/tmp/aeld-data
 FINDER_APP_DIR=$(dirname $0)
 username=$(cat /etc/finder-app/conf/username.txt)
 
@@ -30,29 +30,29 @@ MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines a
 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
-# rm -rf "${WRITEDIR}"
-# 
-# # create $WRITEDIR if not assignment1
-# assignment=`cat /etc/finder-app/conf/assignment.txt`
-# 
-# if [ $assignment != 'assignment1' ]
-# then
-# 	mkdir -p "$WRITEDIR"
-# 
-# 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-# 	#The quotes signify that the entire string in WRITEDIR is a single string.
-# 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-# 	if [ -d "$WRITEDIR" ]
-# 	then
-# 		echo "$WRITEDIR created"
-# 	else
-# 		exit 1
-# 	fi
-# fi
+ rm -rf "${WRITEDIR}"
+ 
+ # create $WRITEDIR if not assignment1
+ assignment=`cat /etc/finder-app/conf/assignment.txt`
+ 
+ if [ $assignment != 'assignment1' ]
+ then
+ 	mkdir -p "$WRITEDIR"
+ 
+ 	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+ 	#The quotes signify that the entire string in WRITEDIR is a single string.
+ 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+ 	if [ -d "$WRITEDIR" ]
+ 	then
+ 		echo "$WRITEDIR created"
+ 	else
+ 		exit 1
+ 	fi
+ fi
 
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+echo "Removing the old writer utility and compiling as a native application"
+make clean
+make
 
 for i in $( seq 1 $NUMFILES)
 do
@@ -67,9 +67,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
+	echo "success" &> /tmp/assignment4-result.txt
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" &> /tmp/assignment4-result.txt
 	exit 1
 fi
